82af26bc1e486d945ab3556c6ae2f614fd42554c
Josh Villbrandt <josh@javconcepts.com>
2014-04-27 00:23:20 -0700

    Add support for non-repo folders

--- batchgit.py
+++ batchgit.py
@@ -14,8 +14,7 @@ parser.add_argument(
     'cmd', action='store', help='the command to run on all repos')
 args = parser.parse_args()
 
-def print_repo_status(name, msg, color):
-    branch = Repo(name).active_branch.__str__()
+def print_repo_status(name, branch, msg, color):
     print('{:30}  {}{:10}{}  {}{}{}'.format(
         name,
         Fore.CYAN, branch, Fore.RESET,
@@ -23,20 +22,25 @@ def print_repo_status(name, msg, color):
 
 # run the command
 if args.cmd == 'status':
-    # print('Running "%s%s%s" on all repos...\n' % (
-    #     Fore.CYAN, args.cmd, Fore.RESET))
+    fetch_fail = False
 
     # list all directories
     dirnames = os.walk('.').next()[1]
     for dirname in dirnames:
         # link to the git repo
-        repo = Repo(dirname)
+        try:
+            repo = Repo(dirname)
+        except Exception:
+            print_repo_status(dirname, "n/a", "not a git repo", Fore.WHITE)
+            continue
+
+        # get active branch
         br = repo.active_branch.__str__()
 
         # find uncommitted files
         if repo.is_dirty():
             print_repo_status(
-                dirname,
+                dirname, br,
                 str(len(repo.index.diff(None))) + " files modified", Fore.RED)
             continue
 
@@ -44,22 +48,31 @@ if args.cmd == 'status':
         unpushed_commits = list(repo.iter_commits('%s@{u}..%s' % (br, br)))
         if len(unpushed_commits) > 0:
             print_repo_status(
-                dirname,
+                dirname, br,
                 "ahead " + str(len(unpushed_commits)) + " commits", Fore.RED)
             continue
 
         # # find out-of-date repos
-        repo.remotes.origin.fetch()
+        try:
+            repo.remotes.origin.fetch()
+        except Exception:
+            # not connected to the network
+            fetch_fail = True
+            pass
         unpulled_commits = list(repo.iter_commits('%s..%s@{u}' % (br, br)))
         if len(unpulled_commits) > 0:
             print_repo_status(
-                dirname,
+                dirname, br,
                 "behind " + str(len(unpulled_commits)) + " commits",
                 Fore.YELLOW)
             continue
 
         # else "ok"
-        print_repo_status(dirname, "ok", Fore.GREEN)
+        print_repo_status(dirname, br, "ok", Fore.GREEN)
+
+    # let the client know if things didn't go well
+    if fetch_fail is True:
+        print("\nUnable to fetch some repositories...")
 else:
     print('Command "%s%s%s" not supported.' % (
         Fore.CYAN, args.cmd, Fore.RESET))
